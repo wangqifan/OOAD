a14972c6fb8c1022d826b3ca5f604e07b09df9f0
wangqifan <1694675518@qq.com>
2020-07-05 16:49:55 +0800

    add images edit sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,3 +27,26 @@ CREATE TABLE loginticket (
                       ticket VARCHAR(256),
                       PRIMARY KEY ( id )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE message (
+                      id INT NOT NULL AUTO_INCREMENT,
+                      from_Id INT NOT NULL,
+                      to_Id INT NOT NULL,
+                      content VARCHAR(256),
+                      has_Read INT,
+                      conversation_Id VARCHAR(256),
+                      created_Date DATE,
+                      PRIMARY KEY ( id )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE comment (
+                      id INT NOT NULL AUTO_INCREMENT,
+                      user_id INT NOT NULL,
+                      content VARCHAR(256),
+                      created_date DATE,
+                      entity_id INT,
+                      entity_type INT,
+                      status INT,
+                      PRIMARY KEY ( id )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
